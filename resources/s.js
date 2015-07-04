c95105727a009294ff8bceff4ddae84396f95b33
Lode Claassen <lode@alsvanzelf.nl>
2015-07-04 22:22:06 +0200

    send forms via zapier

--- resources/s.js
+++ resources/s.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+// highlight the contact form
 $('.goto-contact').on('click', function(){
 	function focusContactForm() {
 		$('#contact .form-group:first-child .form-control').focus();
@@ -19,11 +20,13 @@ $('.goto-contact').on('click', function(){
 	}
 });
 
+// tip popups at some places
 $('.offer .action a').popover({
 	trigger:   'hover focus',
 	placement: 'bottom',
 });
 
+// show more from each offer
 $('.offer').each(function(){
 	$('.panel-body > :not(:first-child)', this).addClass('hidden');
 	$('.panel-body > :first-child', this).append('<a href="#" class="toggler pull-right">Meer &hellip;</a>');
@@ -34,6 +37,33 @@ $('.offer .toggler').on('click', function(event){
 	var offer = $(this).parents('.offer');
 	$('.panel-body .hidden', offer).removeClass('hidden');
 	$(this).remove();
+});
+
+// send the form to zapier
+$('#contact form').on('submit', function(event){
+	event.preventDefault();
+	var form = this;
+	
+	$(form).find('.alert').addClass('hidden');
+	
+	$.ajax({
+		url: 'https://zapier.com/hooks/catch/bwfz7f/',
+		method: 'POST',
+		data: {
+			message: $('#message', form).val(),
+			availability: $('#availability', form).val()
+		}
+	}).done(function(){
+		$('.alert-success', form).removeClass('hidden');
+	}).fail(function(){
+		var email = 'lode@' + window.location.hostname;
+		$('.alert-warning .email', form).html('<a href="mailto:' + email + '">' + email + '</a>');
+		$('.alert-warning', form).removeClass('hidden');
+	}).always(function(){
+		$('html, body').animate({
+			scrollTop: $(form).offset().top + ($(form).height() / 2),
+		}, 100);
+	});
 });
 
 });
